37e889b8e49ed1bbadec1297be073dfd8f701533
Mahbod <mahbod.zamanpour@gmail.com>
2024-05-09 18:56:54 +0330

    update files

--- session1/website/views.py
+++ session1/website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse(' <h1> home page <h2> ')
+    return render(request, 'website/index.html')
 
 def contact_view(request):
-    return HttpResponse(' <h1> contact page <h2> ')
+    return render(request, 'website/contact.html')
 
 def about_view(request):
-    return HttpResponse(' <h1> about page <h2> ')
+    return render(request, 'website/about.html')
