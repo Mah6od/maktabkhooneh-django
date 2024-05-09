3d4ea0086544b50a6f89349f66523137447df046
Mahbod <mahbod.zamanpour@gmail.com>
2024-05-09 18:33:41 +0330

    update files

--- session1/website/views.py
+++ session1/website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index_view(request):
+    return HttpResponse(' <h1> home page <h2> ')
+
+def contact_view(request):
+    return HttpResponse(' <h1> contact page <h2> ')
+
+def about_view(request):
+    return HttpResponse(' <h1> about page <h2> ')
