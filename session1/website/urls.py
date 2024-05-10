34899e74ef74c4ccbd68bfb0816db5f9c572f33f
Mahbod <mahbod.zamanpour@gmail.com>
2024-05-10 07:16:17 +0330

    update files
    settings, urls and htmls

--- session1/website/urls.py
+++ session1/website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
     # urls , view, name
-    path('', index_view),
-    path('contact', contact_view),
-    path('about', about_view),
-
+    path('', index_view, name= 'index'),
+    path('contact', contact_view, name= 'contact'),
+    path('about', about_view, name= 'about'),
 ]
