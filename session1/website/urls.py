182fc22b4057915eebee9a4dd7e69a4a945e892c
Mahbod <mahbod.zamanpour@gmail.com>
2024-05-10 21:02:01 +0330

    update files

--- session1/website/urls.py
+++ session1/website/urls.py
@@ -8,9 +8,4 @@ urlpatterns = [
     path('', index_view, name= 'index'),
     path('contact', contact_view, name= 'contact'),
     path('about', about_view, name= 'about'),
-<<<<<<< HEAD
 ]
-=======
-
-]
->>>>>>> 461abd01afe89a35019fcde90870a98b573b8acf
