4b04eaf1201e685abd8bc3cb034144083367479c
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-02 00:37:27 +0330

    update files

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -11,4 +11,5 @@ class PostAdmin(admin.ModelAdmin):
     # ordering = ['-created_date']
     search_fields = ['title','content']
 
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
 # Create your models here.
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
@@ -8,7 +15,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tags = 
-    # category = 
+    category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -22,3 +29,5 @@ class Post(models.Model):
 
     def __str__(self):
         return " {} - {}".format(self.title, self.id)
+    
+
