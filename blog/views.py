85a5dfed8e766696d52066ea896457b38c55279a
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-02 17:52:14 +0330

    update files

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 # Create your views here.
 def blog_view(request):
     now = timezone.now()
-    posts = Post.objects.filter(status = 1, published_date__lte=now)
+    posts = Post.objects.filter(status = 1,
+                                published_date__lte=now).order_by('published_date')
     context = {"posts":posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -16,8 +17,19 @@ def blog_single(request, pid):
 
     post.counted_views += 1
     post.save()
-    
-    context = {'post':post}
+
+    # Find the index of the current post
+    post_index = list(posts).index(post)
+
+    # Get previous and next posts
+    prev_post = posts[post_index - 1] if post_index > 0 else None
+    next_post = posts[post_index + 1] if post_index < len(posts) - 1 else None
+
+    context = {
+        'post': post,
+        'prev_post': prev_post,
+        'next_post': next_post,
+    }
     return render(request, 'blog/blog-single.html', context)
 
 #def test(request):
