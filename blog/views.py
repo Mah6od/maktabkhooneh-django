96e8d9122f0df7cf26ed664259aaa0e0ccfd3ee3
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-02 17:08:42 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status = 1)
+    now = timezone.now()
+    posts = Post.objects.filter(status = 1, published_date__lte=now)
     context = {"posts":posts}
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    posts = Post.objects.filter(status = 1)
+    now = timezone.now()
+    posts = Post.objects.filter(status = 1, published_date__lte=now)
     post = get_object_or_404(posts, pk=pid)
+
+    post.counted_views += 1
+    post.save()
+    
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
 
-def test(request):
+#def test(request):
     posts = Post.objects.all()
     context = {"posts":posts}
     return render(request, 'test.html', context)
