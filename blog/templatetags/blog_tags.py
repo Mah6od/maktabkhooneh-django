fd1ed0e10526c960865fd75801c579de4b765315
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-10 14:20:39 +0330

    Update blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,17 @@
 from django import template
-
+from blog.models import Post
 register = template.Library()
+
+@register.simple_tag(name='totalposts')
+def function():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+
+@register.simple_tag(name='posts')
+def function():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+
+@register.filter
+def snippet(value):
+    return value[:100]
