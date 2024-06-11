e4bade86610e6936a10d69574204d111e972f0f6
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-11 10:19:02 +0330

    update files

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
+
 register = template.Library()
 
 @register.simple_tag(name='totalposts')
@@ -15,3 +16,20 @@ def function():
 @register.filter
 def snippet(value):
     return value[:100]
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts():
+    posts = Post.objects.filter(status=1).order_by('published_date')[:3]
+    return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+
+    cat_dict = {}
+    for name in categories:
+        posts.filter(category=name)
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories':cat_dict}
+
