9929d82b28e083a8d90575413a37ec3244b276a3
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-17 13:44:59 +0330

    update files

--- website/views.py
+++ website/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from website.forms import NameForm
+from website.forms import NameForm, ContactForm
 
 def index_view(request):
     return render(request, 'website/index.html')
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = ContactForm()
+    return render(request, 'website/contact.html', {'form':form})
 
 def about_view(request):
     return render(request, 'website/about.html')
