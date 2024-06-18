b4dc2c438e3db7c049b38bfc5d8063c18f6ac489
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-18 20:34:29 +0330

    Update files

--- website/forms.py
+++ website/forms.py
@@ -4,17 +4,25 @@ from website.models import Contact, newsletter
 class NameForm(forms.Form):
     name = forms.CharField(max_length=255)
     email = forms.EmailField()
-    subject = forms.CharField(max_length=255)
+    subject = forms.CharField(max_length=255, required=False)  
     message = forms.CharField(widget=forms.Textarea)
 
 class ContactForm(forms.ModelForm):
-    
     class Meta:
         model = Contact
         fields = '__all__'
+        widgets = {
+            'subject': forms.TextInput(attrs={'required': False})  
+        }
+
+    def save(self, commit=True):
+        instance = super(ContactForm, self).save(commit=False)
+        instance.name = 'Unknown'  
+        if commit:
+            instance.save()
+        return instance
 
 class newsletterForm(forms.ModelForm):
-
     class Meta:
         model = newsletter
         fields = '__all__'
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from website.forms import NameForm, ContactForm, newsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -9,20 +10,26 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-
-    form = ContactForm()
-    return render(request, 'website/contact.html', {'form':form})
+            form.save()  # متد save بازنویسی شده مقدار name را به "ناشناس" تغییر می‌دهد
+            messages.success(request, 'Submitted Successfully!')
+        else:
+            messages.error(request, 'There is an Error!')
+    else:
+        form = ContactForm()
+        
+    return render(request, 'website/contact.html', {'form': form})
 
 def newsletter_view(request):
     if request.method == 'POST':
         form = newsletterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Newsletter subscribed successfully!')
             return HttpResponseRedirect('/')
+        else:
+            messages.error(request, 'Subscription failed!')
 
-    else:
-        return HttpResponseRedirect('/')
+    return HttpResponseRedirect('/')
 
 def about_view(request):
     return render(request, 'website/about.html')
