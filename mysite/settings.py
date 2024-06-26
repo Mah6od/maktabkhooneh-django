187c4c135c6d5a04fd9195a72f3c455bc6d02edc
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-26 15:39:26 +0330

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
     'django.contrib.humanize',
+    'django_extensions',
     'website.apps.WebsiteConfig',
     'blog',
 ]
+
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
