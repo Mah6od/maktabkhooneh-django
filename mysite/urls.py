da7a4a9c3dfd8d0e8cb73f30ddc034079ee9e47e
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-30 17:43:06 +0330

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
-
 from website.sitemaps import StaticViewSitemap
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import BlogSitemap
+import debug_toolbar
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -39,6 +39,7 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap"),
         path('robots.txt', include('robots.urls')),
+        path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
