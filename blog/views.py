51039b00e0825633477f3671b7019e75a6e85d83
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-13 17:47:03 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,8 @@ def blog_view(request, **kwargs): # cat_name=None, author_username=None
 
     if kwargs.get('author_username') != None:
             posts = posts.filter(author__username = kwargs['author_username'])
-
+    if kwargs.get('tag_name') != None:
+        posts = posts.filter(tags__name__in=[kwargs['tag_name']])
 
     posts = Paginator(posts, 3)
     try:
