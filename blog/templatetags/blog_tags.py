dd40d3457508b28108234e228a04dbe7aa3be549
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-15 13:51:01 +0330

    Update blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 from django.utils.text import slugify
 import re
 
@@ -14,6 +14,11 @@ def function():
 def function():
     posts = Post.objects.filter(status=1).count()
     return posts
+
+@register.simple_tag(name='comments_count')
+def function(pid):
+    post = Post.objects.get(pk=pid)
+    return Comment.objects.filter(post=post.id, approved=True).count()
 
 @register.filter
 def snippet(value):
