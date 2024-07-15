11ee61f2bc6e4623aefa53bcefe86dfae434744b
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-15 15:38:25 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from blog.models import Post, Comment
 from django.utils import timezone
 from django.utils.text import slugify
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from blog.forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 def blog_view(request, **kwargs): # cat_name=None, author_username=None
@@ -30,11 +32,22 @@ def blog_view(request, **kwargs): # cat_name=None, author_username=None
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
+    form = CommentForm()
+    
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # messages.success(request, 'Submitted Successfully!')
+            messages.add_message(request, messages.SUCCESS, 'Submitted Successfully!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Submitted Successfully!')
+
+    
     now = timezone.now()
     posts = Post.objects.filter(status = 1, published_date__lte=now)
     post = get_object_or_404(posts, pk=pid)
     comments = Comment.objects.filter(post=post.id, approved=True)
-
     post.counted_views += 1
     post.save()
 
@@ -48,6 +61,7 @@ def blog_single(request, pid):
     context = {
         'post': post,
         'comments': comments,
+        'form': form,
         'prev_post': prev_post,
         'next_post': next_post,
     }
