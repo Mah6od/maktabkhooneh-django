831f06485e07037ab5644ab2ff5347910a82a4c6
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-16 15:54:14 +0330

    update files

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog.views import *
+from blog.feeds import LatestEntriesFeed
 
 app_name = 'blog'
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path('category/<str:cat_name>' ,blog_view, name='category'),
     path('tag/<str:tag_name>' ,blog_view, name='tag'),
     path('author/<str:author_username>' ,blog_view, name='author'),
-    path('search/' , blog_search, name='search')
+    path('search/' , blog_search, name='search'),
+    path("rss/feed/", LatestEntriesFeed()),
     #path('test', test, name= 'test'),
 ]
