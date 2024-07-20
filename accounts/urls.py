9aa8498645efe2c8cd5d66737e9dd8dc271833b0
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-20 07:52:08 +0330

    Create urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    # login
+    path('login', views.login_view, name='login'),
+    # logout
+    # path('logout', views.logout_view, name='logout'),
+    # registeration / signup
+    path('signup', views.signup_view, name='signup'),
+]
