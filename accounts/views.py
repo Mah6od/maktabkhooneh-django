380b97dabb9d288764f43e457269af29b26752bf
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-21 14:17:58 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+    
+    else:
+        return redirect('/')
+    form = AuthenticationForm()
+    context = {'form':form}
+    return render(request, 'accounts/login.html', context)
 
 def signup_view(request):
     return render(request, 'accounts/signup.html')
