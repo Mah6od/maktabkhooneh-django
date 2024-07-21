ce01b875fb9b4c34df4fa75ead17b450959d27bb
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-21 14:28:07 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login_view(request):
@@ -20,6 +21,11 @@ def login_view(request):
     form = AuthenticationForm()
     context = {'form':form}
     return render(request, 'accounts/login.html', context)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def signup_view(request):
     return render(request, 'accounts/signup.html')
