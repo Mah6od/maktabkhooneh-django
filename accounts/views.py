da6d550b4b78c557bd32bdd35fd68173636c7f71
Mahbod <mahbod.zamanpour@gmail.com>
2024-07-21 12:45:27 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -13,8 +13,5 @@ def login_view(request):
 
     return render(request, 'accounts/login.html')
 
-# def logout_view(request):
-#    return 
-
 def signup_view(request):
     return render(request, 'accounts/signup.html')
