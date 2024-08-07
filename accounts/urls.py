091c74f5435b9cdd5fcad3c5653d97f8e08f808d
Mahbod <mahbod.zamanpour@gmail.com>
2024-08-07 09:02:10 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
+
 urlpatterns = [
     # login
     path('login/', views.login_view, name='login'),
     # logout
     path('logout/', views.logout_view, name='logout'),
-    # registeration / signup
+    # registration / signup
     path('signup/', views.signup_view, name='signup'),
-    # forgot
-    path('forgot/', views.forgot_view, name='forgot'),
+    # forgot password
+    path('forgot/', auth_views.PasswordResetView.as_view(template_name='accounts/forgot.html'), name='forgot'),
+    path('forgot/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
