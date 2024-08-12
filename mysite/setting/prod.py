6fceaa2491af2dc1bfd0f6ce6d6825c8deb16078
Mahbod <mahbod.zamanpour@gmail.com>
2024-08-12 23:33:24 +0330

    Create prod.py

---
+++ mysite/setting/prod.py
@@ -0,0 +1,37 @@
+from mysite.settings import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-gka0h9%6cq1f65vt8(mzj@es4_pxx4!*n-bgww)x*1i)ak%4&1'
+# CSRF_COOKIE_SECURE = True
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# INSTALLED_APPS = []
+
+# sites framework
+SITE_ID = 2
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+import os
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# CSRF_COOKIE_SECURE = True
